bababdbf5b8b5ec890bb96333087971f86a5af5e
Mehmet Bahadır Maktav <bahadirmaktav314@gmail.com>
2022-01-31 03:40:14 +0300

    New GUI implementation is started with Kivy library.

--- MultilayerAnalysisCode_KivyVersion/multilayerAnalysisApp.py
+++ MultilayerAnalysisCode_KivyVersion/multilayerAnalysisApp.py
@@ -1,6 +1,45 @@
+from msilib.schema import CheckBox
 from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.checkbox import CheckBox
 
-class MultilayerAnalysisApp(App):
+class MainLayout(BoxLayout):
     pass
 
-MultilayerAnalysisApp().run()
+class MaterialProperitiesInputLayout(BoxLayout):
+    pass
+
+class DesignWavelengthInputLayout(BoxLayout):
+    pass
+
+class ThicknessDividerCoefficientLayout(BoxLayout):
+    pass
+
+class WavelengthRangeLayout(BoxLayout):
+    pass
+
+class StepSizeIn1nmLayout(BoxLayout):
+    pass
+
+class MultilayerSequenceLayout(BoxLayout):
+    pass
+
+class LabelWidget(Label):
+    pass
+
+class TextInputWidget(TextInput):
+    pass
+
+class CheckBoxWidget(CheckBox):
+    pass
+
+class MultilayerAnalysisApp(App):
+    def build(self):
+        return MainLayout()
+
+if __name__ == '__main__':
+    Window.clearcolor = (0.08, 0.26, 0.31, 1)
+    MultilayerAnalysisApp().run()
